c34e9dc2c054f9f3d90ac9bad3b4f15548181380
Emlyn O'Regan <emlyn.oregan@healthstream.com>
2023-06-29 18:34:36 +0930

    Latest

--- ui/src/gameexp.js
+++ ui/src/gameexp.js
@@ -2,64 +2,244 @@ import React, { useState, useEffect } from 'react';
 import { 
     Button, Typography, TextField, Grid, CircularProgress 
 } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useSpelunk } from './hooks/spelunk';
-import { useError } from './hooks/error';
-import { GameWindow } from './gamewindow';
+import { GameView } from './gameview';
 
 export const GameExp = () => {
+    const [images, setImages] = useState({});
+
+    const handleLoad = (event) => {
+        const image = event.target;
+
+        setImages(prevImages => {
+            return {
+                ...prevImages,
+                [image.id]: image,
+            };
+        });
+    };
+
     return <>
-    <img id="image1" src="path/to/your/image1.png" alt="" style={{ display: 'none' }} />
+        <img id="terrain" src="./terrain_tiles_v2.png" style={{ display: 'none' }} onLoad={handleLoad} />
+        <img id="char" src="./Character_007.png" style={{ display: 'none' }} onLoad={handleLoad} />
             
-        <img id="grass" src="./grass.png" style={{ display: 'none' }} />
-        <img id="mountain" src="./mountain.png" style={{ display: 'none' }} />
-        <img id="tree" src="./tree.png" style={{ display: 'none' }} />
-        <GameExpInner />
+        {/* <img id="grass" src="./grass.png" style={{ display: 'none' }} onLoad={handleLoad} />
+        <img id="mountain" src="./mountain.png" style={{ display: 'none' }} onLoad={handleLoad} />
+        <img id="tree" src="./tree.png" style={{ display: 'none' }} onLoad={handleLoad} /> */}
+        <GameExpInner images={images} />
     </>
 };
 
-const GameExpInner = () => {
-    const tree = document.getElementById("tree");
-    const grass = document.getElementById("grass");
-    const mountain = document.getElementById("mountain");
+const GameExpInner = ({images}) => {
+    const imageList = [images.terrain, images.char];
 
-    const images = [grass, mountain, tree];
-    const sprites = [
-        {
-            image_index: 0,
-            position: { x: 0, y: 0 },
-            size: 100,
-            rotation: 0,
-        },
-        {
+    // 0 = tree, 1 = grass, 2 = mountain
+    // the map is mostly grass, with a few trees and mountains
+    const tileIndices = [
+        [55, 55, 55, 55, 55, 55, 55, 55, 55, 55], // 0
+        [55, 55, 55, 55, 55, 55, 55, 55, 55, 55], // 1
+        [55, 55, 55, 55, 55, 55, 55, 55, 55, 55], // 2
+        [55, 55, 55, 0, 1, 1, 2, 55, 55, 55], // 3
+        [55, 55, 55, 10, 11, 11, 12, 55, 55, 55], // 4
+        [55, 55, 55, 10, 11, 11, 12, 55, 55, 55], // 5
+        [55, 55, 55, 20, 42, 40, 22, 55, 55, 55], // 6
+        [55, 55, 55, 55, 10, 12, 55, 55, 55, 55], // 7
+        [55, 55, 55, 55, 10, 12, 55, 55, 55, 55], // 8
+        [55, 55, 55, 55, 10, 12, 55, 55, 55, 55]  // 9
+    ]
+
+    const [mapTiles, setMapTiles] = useState([]);
+
+    const mapTopLeft = { x: 0, y: 0 };
+    const mapBottomRight = { x: 1000, y: 1000 };
+    const [viewCenter, setViewCenter] = useState({ x: 500, y: 500 });
+    const [viewSize, setViewSize] = useState(1100); 
+
+    const viewTopLeft = {
+        x: viewCenter.x - viewSize / 2,
+        y: viewCenter.y - viewSize / 2,
+    };
+
+    const viewBottomRight = {
+        x: viewCenter.x + viewSize / 2,
+        y: (viewCenter.y + viewSize / 2)
+    };
+
+    useEffect(() => {
+        const newMapTiles = [];
+        for (let y = 0; y < tileIndices.length; y++) {
+            for (let x = 0; x < tileIndices[y].length; x++) {
+                const tileNum = tileIndices[y][x];
+                // calculate the tile's position in the tileset image
+                const sx = ((tileNum % 10) * 32);
+                const sy = (Math.floor(tileNum / 10) * 32)+1;
+
+                newMapTiles.push({
+                    image_index: 0,
+                    position: { x: x * 100, y: y * 100 },
+                    size: 100,
+                    rotation: 0,
+                    sx,
+                    sy,
+                    sWidth: 30,
+                    sHeight: 30
+                });
+            }
+        }
+        setMapTiles(newMapTiles);
+    }, []);
+
+
+    // when the user presses the arrow keys, move the view
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'ArrowLeft') {
+                setViewCenter((prevViewCenter) => ({
+                    ...prevViewCenter,
+                    x: prevViewCenter.x - 10,
+                }));
+            } else if (event.key === 'ArrowRight') {
+                setViewCenter((prevViewCenter) => ({
+                    ...prevViewCenter,
+                    x: prevViewCenter.x + 10,
+                }));
+            } else if (event.key === 'ArrowUp') {
+                setViewCenter((prevViewCenter) => ({
+                    ...prevViewCenter,
+                    y: prevViewCenter.y - 10,
+                }));
+            } else if (event.key === 'ArrowDown') {
+                setViewCenter((prevViewCenter) => ({
+                    ...prevViewCenter,
+                    y: prevViewCenter.y + 10,
+                }));
+            } else if (event.key === '+') {
+                setViewSize((prevViewSize) => prevViewSize - 10);
+            } else if (event.key === '-') {
+                setViewSize((prevViewSize) => prevViewSize + 10);
+            }
+        };
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    // now add the character
+    // the character is 32x32 pixels, and the tile map is 16 tiles (each 32x32), 4 x 4.
+
+    const [charPosition, setCharPosition] = useState({ x: 500, y: 500 });
+    const charSize = 75; // in game units
+    const [charDirection, setCharDirection] = useState('down');
+    const [charAnimation, setCharAnimation] = useState(0); // 0 to 3
+    // recalculate the view center based on the character's position
+    // min is left/top, max is right/bottom
+    
+
+    useEffect(() => {
+        const viewCenterX = Math.max(Math.min(charPosition.x, mapBottomRight.x), mapTopLeft.x); 
+        const viewCenterY = Math.max(Math.min(charPosition.y, mapBottomRight.y), mapTopLeft.y);
+        setViewCenter({ x: viewCenterX, y: viewCenterY });
+        // console.log(viewCenterX, viewCenterY);
+    }, [charPosition]);
+    
+
+    // drive the character with asdw
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'a') {
+                // check if the character is at the edge of the map
+                console.log(charPosition.x, mapTopLeft.x);
+                if (charPosition.x > mapTopLeft.x+(charSize/2)) {
+                    setCharPosition((prevCharPosition) => ({
+                        ...prevCharPosition,
+                        x: prevCharPosition.x - 10,
+                    }));
+                }
+                setCharDirection('left');
+                setCharAnimation((prevCharAnimation) => (prevCharAnimation + 1) % 4);
+            } else if (event.key === 'd') {
+                console.log(charPosition.x, mapBottomRight.x);
+                if (charPosition.x < mapBottomRight.x-(charSize/2)) {
+                    setCharPosition((prevCharPosition) => ({
+                        ...prevCharPosition,
+                        x: prevCharPosition.x + 10,
+                    }));
+                }
+                setCharDirection('right');
+                setCharAnimation((prevCharAnimation) => (prevCharAnimation + 1) % 4);
+            } else if (event.key === 'w') {
+                if (charPosition.y > mapTopLeft.y+(charSize/2)) {
+                    setCharPosition((prevCharPosition) => ({
+                        ...prevCharPosition,
+                        y: prevCharPosition.y - 10,
+                    }));
+                }
+                setCharDirection('up');
+                setCharAnimation((prevCharAnimation) => (prevCharAnimation + 1) % 4);
+            } else if (event.key === 's') {
+                if (charPosition.y < mapBottomRight.y-(charSize/2)) {
+                    setCharPosition((prevCharPosition) => ({
+                        ...prevCharPosition,
+                        y: prevCharPosition.y + 10,
+                    }));
+                }
+                setCharDirection('down');
+                setCharAnimation((prevCharAnimation) => (prevCharAnimation + 1) % 4);
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [charPosition]);
+
+    const [charTiles, setCharTiles] = useState([]);
+
+    useEffect(() => {
+        // calculate the tile's position in the tileset image
+        const row = charDirection === 'down' ? 0 : charDirection === 'left' ? 1 : charDirection === 'right' ? 2 : 3;
+        const column = charAnimation;
+
+        const sx = (column * 48);
+        const sy = (row * 48)+1;
+        const sWidth = 48;
+        const sHeight = 48;
+
+        setCharTiles([{
             image_index: 1,
-            position: { x: 100, y: 0 },
-            size: 100,
+            position: { x: charPosition.x - charSize/2, y: charPosition.y - charSize/2 },
+            size: charSize,
             rotation: 0,
-        },
-        {
-            image_index: 2,
-            position: { x: 200, y: 0 },
-            size: 100,
-            rotation: 0,
-        },
-    ];
+            sx,
+            sy,
+            sWidth,
+            sHeight
+        }]);
+    }, [charPosition, charDirection, charAnimation]);
 
     return (
         <>
-        <Grid container spacing={2} justifyContent={'center'}>
-            <Grid container spacing={2} 
-                justifyContent="center" 
-                sx = {{padding: 7}}
-                maxWidth="lg"
-                // direction="column"
-                alignItems="center"
-            >
-                <Grid item xs={12} sx={{ height: '800px' }}>
-                    <GameWindow images={images} sprites={sprites} topLeft={{x:50, y:0}} bottomRight={{x:300, y:300}} />
+            <Grid container spacing={2} justifyContent={'center'}>
+                <Grid container spacing={2} 
+                    justifyContent="center" 
+                    sx = {{padding: 7}}
+                    maxWidth="lg"
+                    // direction="column"
+                    alignItems="center"
+                >
+                    <Grid item xs={6} sx={{ height: '500px'}}>
+                        <GameView
+                            images={imageList} 
+                            sprites={[mapTiles, charTiles]}
+                            viewTopLeft={viewTopLeft}
+                            viewBottomRight={viewBottomRight}
+                            mapTopLeft={mapTopLeft}
+                            mapBottomRight={mapBottomRight}
+                        />
+                    </Grid>
                 </Grid>
             </Grid>
-        </Grid>
         </>
     );
 };
---
+++ ui/src/gameview.js
@@ -0,0 +1,249 @@
+import React, { useRef, useEffect } from 'react';
+
+
+const GameView = ({ images, sprites, viewTopLeft, viewBottomRight, mapTopLeft, mapBottomRight }) => {
+/* 
+    images is a list of image objects. An image must be suitable for using with the canvas drawImage 
+    function. Allowed image types are HTMLImageElement, SVGImageElement, HTMLVideoElement, HTMLCanvasElement, 
+    ImageBitmap, OffscreenCanvas, ImageData, or SVGImageElement.
+    
+    sprites is a list of lists or sprite objects. The lists can in turn be lists or sprite objects, 
+    transitively as deep as you like. A sprite object must have the following properties:
+        image_index: The index of the image in the images list to draw for this sprite.
+        position: An object with x and y properties, giving the position of the sprite in the game world.
+        size: The size of the sprite in the game world.
+        rotation: The rotation of the sprite in degrees.
+    The sprites are drawn in the order they appear in the list, using in-order traversal of inner lists.
+
+    viewTopLeft and viewBottomRight are the top left and bottom right corners of this view in the game world.
+
+    mapTopLeft and mapBottomRight are the top left and bottom right corners of the whole map in the game world. We 
+    will paint black outside this area.
+
+    Note that all coordinates are Game Coordinates, ie: a self consistent coordinate system that is independent
+    
+*/
+    const canvasRef = useRef(null);
+    const containerRef = useRef(null);
+    const animationFrameIdRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        const container = containerRef.current;
+
+        let resizeTimeout;
+
+        const GameSizeToCanvasSize = canvas.width / (viewBottomRight.x - viewTopLeft.x);
+
+        const ratioHeightToWidth = canvas.height / canvas.width;
+
+        const adjustedViewBottom = viewTopLeft.y + (viewBottomRight.x - viewTopLeft.x) * ratioHeightToWidth;
+
+        // Function to update canvas size
+        const updateCanvasSize = () => {
+            if (resizeTimeout) {
+                clearTimeout(resizeTimeout);
+            }
+
+            resizeTimeout = setTimeout(() => {
+                if (container) {
+                    canvas.width = container.offsetWidth;
+                    canvas.height = container.offsetHeight;
+                }
+            }, 100);
+        };
+
+        
+        const GameXToCanvasX = (gameX) => {
+            const canvasX = (gameX - viewTopLeft.x) * GameSizeToCanvasSize;
+            return canvasX;
+        };
+
+        const GameXSizeToCanvasXSize = (gameXSize) => {
+            const canvasXSize = gameXSize * GameSizeToCanvasSize;
+            return canvasXSize;
+        };
+
+        const GameYToCanvasY = (gameY) => {
+            const canvasY = (gameY - viewTopLeft.y) * GameSizeToCanvasSize; // * canvas.height / (viewBottomRight.y - viewTopLeft.y);
+            return canvasY;
+        };
+
+        const GameYSizeToCanvasYSize = (gameYSize) => {
+            const canvasYSize = gameYSize * GameSizeToCanvasSize; //canvas.height / (viewBottomRight.y - viewTopLeft.y);
+            return canvasYSize;
+        };
+        
+        const drawBlackBackground = () => {
+            context.save();
+            
+            context.fillStyle = 'black';
+
+            // We need to detect if any of the outside of the map is visible, and if so, draw it black.
+            
+            // First, if the view is entirely outside the map, then we can just draw the whole canvas black.
+
+            if (viewBottomRight.x < mapTopLeft.x || viewTopLeft.x > mapBottomRight.x || adjustedViewBottom < mapTopLeft.y || viewTopLeft.y > mapBottomRight.y) {
+                context.fillRect(0, 0, canvas.width, canvas.height);
+            }
+            else
+            {
+                // If the top of the map (mapTopLeft.y) is below the top of the view (viewTopLeft.y), then we need to draw
+                // a black rectangle from the top of the view to the top of the map.
+
+                if (mapTopLeft.y > viewTopLeft.y) {
+                    const rectLeftCanvas = 0;
+                    const rectTopCanvas = 0;
+                    const rectWidthCanvas = canvas.width;
+                    const rectHeightCanvas = GameYToCanvasY(mapTopLeft.y - viewTopLeft.y);
+                    context.fillRect(rectLeftCanvas, rectTopCanvas, rectWidthCanvas, rectHeightCanvas);
+                }
+
+                // If the bottom of the map (mapBottomRight.y) is above the bottom of the view (viewBottomRight.y), then we need to draw
+                // a black rectangle from the bottom of the map to the bottom of the view.
+
+                if (mapBottomRight.y < adjustedViewBottom) {
+                    const rectLeftCanvas = 0;
+                    const rectTopCanvas = GameYToCanvasY(mapBottomRight.y);
+                    const rectWidthCanvas = canvas.width;
+                    const rectHeightCanvas = canvas.height - rectTopCanvas;
+                    context.fillRect(rectLeftCanvas, rectTopCanvas, rectWidthCanvas, rectHeightCanvas);
+                }
+
+                // If the left of the map (mapTopLeft.x) is to the right of the left of the view (viewTopLeft.x), then we need to draw
+                // a black rectangle from the left of the view to the left of the map.
+
+                if (mapTopLeft.x > viewTopLeft.x) {
+                    const rectLeftCanvas = 0;
+                    const rectTopCanvas = 0;
+                    const rectWidthCanvas = GameXToCanvasX(mapTopLeft.x - viewTopLeft.x);
+                    const rectHeightCanvas = canvas.height;
+                    context.fillRect(rectLeftCanvas, rectTopCanvas, rectWidthCanvas, rectHeightCanvas);
+                }
+
+                // If the right of the map (mapBottomRight.x) is to the left of the right of the view (viewBottomRight.x), then we need to draw
+                // a black rectangle from the right of the map to the right of the view.
+
+                if (mapBottomRight.x < viewBottomRight.x) {
+                    const rectLeftCanvas = GameXToCanvasX(mapBottomRight.x);
+                    const rectTopCanvas = 0;
+                    const rectWidthCanvas = canvas.width - rectLeftCanvas;
+                    const rectHeightCanvas = canvas.height;
+                    context.fillRect(rectLeftCanvas, rectTopCanvas, rectWidthCanvas, rectHeightCanvas);
+                }
+            }
+
+            context.restore();
+        };
+
+
+
+        // Function to draw sprites on the canvas
+        const drawSprites = () => {
+            const flattenedSprites = sprites.flat(Infinity);
+
+            flattenedSprites.forEach(sprite => {
+                const image = images[sprite.image_index];
+
+                const sprite_width = sprite.sWidth || image.width;
+                const sprite_height = sprite.sHeight || image.height;
+                const sprite_sx = sprite.sx || 0;
+                const sprite_sy = sprite.sy || 0;
+                // console.log(image)
+                // console.log(sprite);
+
+                // detect if the sprite is outside the view, and if so, don't draw it.
+
+                // console.log(images)
+
+                if (image && !(sprite.position.x + sprite.size < viewTopLeft.x || sprite.position.x > viewBottomRight.x || sprite.position.y + sprite.size < viewTopLeft.y || sprite.position.y > adjustedViewBottom)) {
+
+                    // Save the current state
+                    context.save();
+
+                    // // Apply rotation
+                    // const centerX = sprite.position.x + sprite.size / 2;
+                    // const centerY = sprite.position.y + sprite.size / 2;
+                    // context.translate(centerX, centerY);
+                    // context.rotate((sprite.rotation * Math.PI) / 180);
+                    // context.translate(-centerX, -centerY);
+
+                    // we have two coordinate systems.
+
+                    // The sprites, and the top left and bottom right  are in Game coordinates.
+                    // The canvas is in canvas coordinates.
+
+                    // We need to translate the sprite position from Game coordinates to canvas coordinates.
+
+                    const x_window = GameXToCanvasX(sprite.position.x);
+                    const y_window = GameYToCanvasY(sprite.position.y);
+                    const x_size_window = GameXSizeToCanvasXSize(sprite.size);
+                    const y_size_window = GameYSizeToCanvasYSize(sprite.size);
+                    
+                    // Draw image
+                    context.drawImage(
+                        image,
+                        sprite_sx,
+                        sprite_sy,
+                        sprite_width,
+                        sprite_height,
+                        x_window,
+                        y_window,
+                        x_size_window+1,
+                        y_size_window+1
+                    );
+
+                    // Restore to the state
+                    context.restore();
+                }
+            });
+        };
+
+        // Function to update the content of the canvas
+        const updateContent = () => {
+            // Clear the entire canvas
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Draw a black background
+            drawBlackBackground();
+
+            // Draw sprites
+            drawSprites();
+
+            // Draw a black border around the canvas
+            context.strokeStyle = 'black';
+            context.lineWidth = 1;
+            context.strokeRect(0, 0, canvas.width, canvas.height);
+
+            // Request the next animation frame
+            animationFrameIdRef.current = requestAnimationFrame(updateContent);
+        };
+
+        // Initial update of canvas size
+        updateCanvasSize();
+
+        // Start the animation loop
+        animationFrameIdRef.current = requestAnimationFrame(updateContent);
+
+        // Watch for container resize and update canvas size
+        const resizeObserver = new ResizeObserver(updateCanvasSize);
+        resizeObserver.observe(container);
+
+        // Cleanup
+        return () => {
+            resizeObserver.disconnect();
+            cancelAnimationFrame(animationFrameIdRef.current);
+            clearTimeout(resizeTimeout);
+        };
+
+    }, [images, sprites]);
+
+    return (
+        <div ref={containerRef} style={{ width: '100%', height: '100%' }}>
+            <canvas ref={canvasRef} style={{ display: 'block' }} />
+        </div>
+    );
+};
+
+export {GameView};
